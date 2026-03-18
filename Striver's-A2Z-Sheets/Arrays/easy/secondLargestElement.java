fc5f108fba1c26619fe595d1125239fc5af01757
abdur4code <abdurrahim0101001@gmail.com>
2026-03-18 21:28:51 +0530

    Add solution to find the second largest element in an array

---
+++ Striver's-A2Z-Sheets/Arrays/easy/secondLargestElement.java
@@ -0,0 +1,41 @@
+//==================Second Largest Element================
+//Given an array of integers nums, return the second-largest element in the array. If the second-largest element does not exist, return -1.
+//
+//Example 1
+//Input: nums = [8, 8, 7, 6, 5]
+//Output: 7
+//Explanation:
+//The largest value in nums is 8, the second largest is 7
+//Example 2
+//Input: nums = [10, 10, 10, 10, 10]
+//Output: -1
+//Explanation:
+//The only value in nums is 10, so there is no second largest value, thus -1 is returned
+
+class Solution {
+    public int secondLargestElement(int[] nums) {
+
+        int max = nums[0];
+        int size = nums.length;
+
+        for (int i=1; i<size; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+
+        int max2 = Integer.MIN_VALUE;
+
+        for(int j = 0; j < size; j++){
+            if (nums[j] < max && nums[j] > max2){
+                max2 = nums[j];
+            }
+        }
+        if(max2 == Integer.MIN_VALUE){
+            return -1;
+        }
+
+        return max2;
+
+    }
+}
