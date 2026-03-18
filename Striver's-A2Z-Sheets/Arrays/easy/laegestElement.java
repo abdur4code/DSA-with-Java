15f0c5d5044604401c073ecb92970e11acd2d03d
abdur4code <abdurrahim0101001@gmail.com>
2026-03-18 21:25:12 +0530

    Add solution to find the largest element in an array

---
+++ Striver's-A2Z-Sheets/Arrays/easy/laegestElement.java
@@ -0,0 +1,26 @@
+//============Largest Element===========
+//Given an array of integers nums, return the value of the largest element in the array
+//
+//Example 1
+//Input: nums = [3, 3, 6, 1]
+//Output: 6
+//Explanation: The largest element in array is 6
+//Example 2
+//Input: nums = [3, 3, 0, 99, -40]
+//Output: 99
+//Explanation: The largest element in array is 99
+
+
+class Solution {
+    public int largestElement(int[] nums) {
+
+        int large = nums[0];
+
+        for (int i =0; i < nums.length; i++){
+            if(large < nums[i]){
+                large = nums[i];
+            }
+        }
+        return large;
+    }
+}
