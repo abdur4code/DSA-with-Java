bcffee0fdf789389f64d10f6584c03c70959aa54
abdur4code <abdurrahim0101001@gmail.com>
2026-04-29 01:23:26 +0530

    Sol: Optimal solution for Array Leaders problem using right-to-left traversal with TC = O(n) and SC = O(n)

--- Striver's-A2Z-Sheets/Arrays/medium/arrayLeaders.java
+++ Striver's-A2Z-Sheets/Arrays/medium/arrayLeaders.java
@@ -51,4 +51,23 @@ class Solution {
     }
 }
 
+// ============Optimal Approach=============
 
+class Solution2 {
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        ArrayList<Integer> result = new ArrayList<>();
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = n-1; i >= 0; i--){
+            if(arr[i] >= max){
+                result.add(arr[i]);
+            }
+            max = Math.max(max, arr[i]);
+        }
+        
+        Collections.reverse(result);
+        return result;
+    }
+}
