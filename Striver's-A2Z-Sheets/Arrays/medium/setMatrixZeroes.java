418fe4864e55cb97a1f4adaa7ee530e162b9fc42
abdur4code <abdurrahim0101001@gmail.com>
2026-06-14 16:10:44 +0530

    Optimal sol: For Set Matrix Zeroes problem with O(m*n) time complexity and O(1) space complexity

--- Striver's-A2Z-Sheets/Arrays/medium/setMatrixZeroes.java
+++ Striver's-A2Z-Sheets/Arrays/medium/setMatrixZeroes.java
@@ -111,3 +111,47 @@ class setMatrixZeroes2 {
         }
     }
 }
+
+//Optimal Solution
+//Time Complexity: O(m*n) Space Complexity: O(1)
+class setMatrixZeroes3 {
+    public void setZeroes(int[][] matrix) {
+        //Optimal Solution
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = 1;
+
+        for(int i = 0; i< m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if(j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i = m-1; i > 0; i--) {
+            for(int j = n-1; j > 0; j--) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int j = n-1; j > 0; j--) {
+            if(matrix[0][0] == 0) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            if(col0 == 0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
