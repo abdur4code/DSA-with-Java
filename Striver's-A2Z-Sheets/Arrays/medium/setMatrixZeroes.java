5dbefc0ddf2cf3a25be92a6ca70ecfe04b90d1cd
abdur4code <abdurrahim0101001@gmail.com>
2026-06-14 16:05:09 +0530

    Brute Force sol: For Set Matrix Zeroes problem with O(m*n*(m+n)) time complexity

---
+++ Striver's-A2Z-Sheets/Arrays/medium/setMatrixZeroes.java
@@ -0,0 +1,79 @@
+// 73. Set Matrix Zeroes
+/*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+ 
+
+Follow up:
+
+A straightforward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+*/
+
+//Brute Force
+//Time Complexity: O(m*n*(m+n)) Space Complexity: O(1) 
+public class setMatrixZeroes {
+     public void setZeroes(int[][] matrix) {
+        //Brute Force
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    markRow(matrix, i);
+                    markCol(matrix, j);
+                }
+            }
+        }
+
+         for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == -1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    private static void markRow(int[][] matrix, int i) {
+        int n = matrix[0].length;
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != 0) {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+     private static void markCol(int[][] matrix, int j) {
+        int m = matrix.length;
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][j] != 0) {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+}
